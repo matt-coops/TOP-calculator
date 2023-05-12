5d72d2205665efafae086443ab659f8021640898
matt-coops <matt.cooper.85@gmail.com>
2023-05-12 13:43:39 +1000

    Finish required functionality.

--- Javascript.js
+++ Javascript.js
@@ -1,6 +1,7 @@
 "use strict";
 
-const containerButtons = document.querySelector(".buttons");
+const containerSquareButtons = document.querySelector(".square--buttons");
+const containerRectButtons = document.querySelector(".rect--buttons");
 const containerMainScreen = document.querySelector(".main--screen");
 const containerTopScreen = document.querySelector(".top--screen");
 let firstNum = "";
@@ -21,11 +22,36 @@ const operate = function (a, b, c) {
   if (b === "+") return a + c;
   if (b === "-") return a - c;
   if (b === "*") return a * c;
-  if (b === "/") return a / c;
+  if (b === "%") return a / c;
 };
 
-containerButtons.addEventListener("click", function (e) {
-  if (e.target.classList.contains("buttons")) return;
+containerRectButtons.addEventListener("click", function (e) {
+  if (e.target.classList.contains("rect--buttons")) return;
+  if (e.target.classList.contains("button--clear")) init();
+  if (
+    e.target.classList.contains("button--delete") &&
+    firstNum === "" &&
+    secondNum === ""
+  )
+    return;
+  if (
+    e.target.classList.contains("button--delete") &&
+    firstNum !== "" &&
+    containerTopScreen.textContent === ""
+  ) {
+    firstNum = firstNum.slice(0, -1);
+    containerMainScreen.textContent = firstNum;
+  } else if (
+    e.target.classList.contains("button--delete") &&
+    secondNum !== ""
+  ) {
+    secondNum = secondNum.slice(0, -1);
+    containerMainScreen.textContent = `${operator} ${secondNum}`;
+  } else return;
+});
+
+containerSquareButtons.addEventListener("click", function (e) {
+  if (e.target.classList.contains("square--buttons")) return;
   if (e.target.classList.contains("button--digit") && operator === "") {
     if (firstNum.length === 10) firstNum = firstNum.slice(0, -1);
     firstNum += e.target.dataset.button;
@@ -44,12 +70,11 @@ containerButtons.addEventListener("click", function (e) {
     containerTopScreen.textContent = `${firstNum}`;
     containerMainScreen.textContent = `${operator}`;
   }
-  if (e.target.classList.contains("button--clear")) init();
   if (e.target.classList.contains("button--equals")) {
     if (!firstNum || !operator || !secondNum) return;
     containerTopScreen.textContent = `${firstNum} ${operator} ${secondNum}`;
-    result = operate(+firstNum, operator, +secondNum);
-    containerMainScreen.textContent = result;
+    result = operate(+firstNum, operator, +secondNum).toFixed(6);
+    containerMainScreen.textContent = +result;
     firstNum = result;
     operator = "";
     secondNum = "";
